9beb45d03060f666f8677fac23de079b3709d9eb
Syddev <sydneysantos176@gmail.com>
2025-12-16 16:12:56 +0800

    adjust padding top of chat are

--- client/src/pages/Index.jsx
+++ client/src/pages/Index.jsx
@@ -413,7 +413,7 @@ const Index = () => {
             </div>
 
             {/* Messages Area */}
-            <div className="flex-1 overflow-y-auto p-4 space-y-4 pt-36">
+            <div className="flex-1 overflow-y-auto p-4 space-y-4 pt-6">
               {messages.map((msg, idx) => (
                 <div
                   key={idx}
