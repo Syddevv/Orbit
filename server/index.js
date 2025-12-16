5e7f78ed6518d907cfc9d2c5b6e4a77d4bd75fe0
Syddev <sydneysantos176@gmail.com>
2025-12-16 15:01:06 +0800

    Fix: bind server to dynamic process.env.PORT for Render

--- server/index.js
+++ server/index.js
@@ -126,6 +126,8 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(5000, () => {
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
   console.log("SERVER RUNNING ON PORT 5000");
 });
